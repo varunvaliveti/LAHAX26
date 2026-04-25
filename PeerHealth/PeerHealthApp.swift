0f62ea6916daa36856ad90561112b0f83f690e65
Varun <valivetiavrun@gmail.com>
2026-04-25 15:34:37 -0700

    Initial Commit

---
+++ PeerHealth/PeerHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeerHealthApp.swift
+//  PeerHealth
+//
+//  Created by Varun Valiveti on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PeerHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PeerHealthTests/PeerHealthTests.swift
@@ -0,0 +1,19 @@
+//
+//  PeerHealthTests.swift
+//  PeerHealthTests
+//
+//  Created by Varun Valiveti on 4/25/26.
+//
+
+import Testing
+@testable import PeerHealth
+
+struct PeerHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
